451abea497dc37e23de91f7866ac5ec91958de6d
MAC <mac@MacBook-Pro.local>
2025-12-21 23:01:27 +0700

    add frontend user

--- clone_repos.sh
+++ clone_repos.sh
@@ -7,3 +7,4 @@ git clone https://github.com/thobui14ig/frontend-seo.git
 git clone https://github.com/thobui14ig/backend-service.git
 git clone https://github.com/thobui14ig/cronjob-service.git
 git clone https://github.com/thobui14ig/comment-service.git
+git clone https://github.com/thobui14ig/frontend-user.git
