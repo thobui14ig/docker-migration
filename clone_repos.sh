452e5962a69356003ee6eb874e144cb17f3c5a9c
MAC <mac@192.168.1.32>
2025-12-21 14:43:14 +0700

    [update] add repo service

---
+++ clone_repos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Lui ra một thư mục so với vị trí của file .sh
+cd ..
+
+# Clone các repository
+git clone https://github.com/thobui14ig/frontend-seo.git
+git clone https://github.com/thobui14ig/backend-service.git
+git clone https://github.com/thobui14ig/cronjob-service.git
+git clone https://github.com/thobui14ig/comment-service.git
